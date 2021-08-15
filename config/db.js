fceaba32a9158a373b3f2f2d7cb2368bc7b3527e
unknown <brayan_silva82151@elpoli.edu.co>
2021-08-15 13:30:43 -0500

    Deployment parche

--- config/db.js
+++ config/db.js
@@ -5,11 +5,19 @@ module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER , proc
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
+    ssl: true,
+    protocol: "postgres",
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
     // define:{
     //     timestamps: false
